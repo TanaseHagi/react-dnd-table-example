3de51099ab3260dfde826893cb6dcf9a63845137
Tanase <tanase.hagi@teamart.ro>
2019-06-14 18:15:09 +0300

    Updates

---
+++ precache-manifest.331ea653b15520595ecdf7b09268d48b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4485e892a1570dae54af894aa7f5c882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07f37150b6878c0e9b3",
+    "url": "/static/js/2.70a1c652.chunk.js"
+  },
+  {
+    "revision": "ba95e66da619254595b0",
+    "url": "/static/js/main.7ff0fc11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
